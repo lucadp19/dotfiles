569c83f3d569306c1522527be715cd6da0a2c0b4
lucadp19 <luca.dp1909@gmail.com>
2021-03-10 22:49:33 +0100

    Modified polybar and picom configs

--- autostart/.startup-scripts/polybar-startup.sh
+++ autostart/.startup-scripts/polybar-startup.sh
@@ -6,5 +6,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-polybar --reload xmonadbar &
+polybar --config=$HOME/.config/polybar/config.ini --reload xmonadbar &
 
